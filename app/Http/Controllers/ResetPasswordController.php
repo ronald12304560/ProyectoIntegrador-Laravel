e7303c972997e6cf5d5b3e3c98397cd5d7598a16
Annderson <ronald.lopez@upeu.edu.pe>
2020-06-14 12:26:53 -0500

    Inicio

---
+++ app/Http/Controllers/ResetPasswordController.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\User;
+use Symfony\Component\HttpFoundation\Response;
+use Illuminate\Support\Facades\Mail;
+use App\Mail\ResetPasswordMail;
+use Carbon\Carbon;
+use Illuminate\Support\Facades\DB;
+
+class ResetPasswordController extends Controller
+{
+    public function sendEmail(Request $request)
+    {
+        if (!$this->validateEmail($request->email)) {
+            return $this->failedResponse();
+        }
+
+        $this->send($request->email);
+        return $this->successResponse();
+    }
+
+    public function send($email)
+    {
+        $token = $this->createToken($email);
+        Mail::to($email)->send(new ResetPasswordMail($token));
+    }
+
+    public function createToken($email)
+    {
+        $oldToken = DB::table('password_resets')->where('email', $email)->first();
+
+        if ($oldToken) {
+            return $oldToken->token;
+        }
+
+        $token = str_random(60);
+        $this->saveToken($token, $email);
+        return $token;
+    }
+
+    public function saveToken($token, $email)
+    {
+        DB::table('password_resets')->insert([
+            'email' => $email,
+            'token' => $token,
+            'created_at' => Carbon::now()
+        ]);
+    }
+
+    public function validateEmail($email)
+    {
+        return !!User::where('email', $email)->first();
+    }
+
+    public function failedResponse()
+    {
+        return response()->json([
+            'error' => 'Email no encontrado en la BD'
+        ], Response::HTTP_NOT_FOUND);
+    }
+
+    public function successResponse()
+    {
+        return response()->json([
+            'data' => 'Se Cambio la contraseña, Revisa tu Email'
+        ], Response::HTTP_OK);
+    }
+}
